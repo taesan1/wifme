6508ae890ed66af08eccf0ab3b216f33a6f29640
taesan1 <hts1304@naver.com>
2023-09-12 23:59:29 +0000

    a

--- public/tw/135/test/cbsdodge.js
+++ public/tw/135/test/cbsdodge.js
@@ -45,12 +45,13 @@ var axe = parseInt(document.forms[0].axe.nextSibling.nextSibling.innerHTML.match
 var ram = parseInt(document.forms[0].ram.nextSibling.nextSibling.innerHTML.match(/\d+/));
 var snob = parseInt(document.forms[0].snob.nextSibling.nextSibling.innerHTML.match(/\d+/));
 var knight = parseInt(document.forms[0].knight.nextSibling.nextSibling.innerHTML.match(/\d+/));
-if (light > 0 || catapult > 10 || archer>0|| heavy > 0 || spy>10 || spear > 0 || sword > 0 || axe > 0 || snob > 0) {
+if (light > 0 || catapult > 10 || archer>2|| heavy > 0 || spy>10 || spear>2 || sword>2 || axe > 0 || snob > 0) {
 
-    if (spy>2) {spy=spy-3; }else{ spy=0;};
+    if (spy>2){spy=spy-3;}else{ spy=0;};
     if (catapult>10){ catapult=catapult-10;} else {catapult=0;};
     if (spear>2){spear=spear-2;}else{spear=0;}
     if (sword>2){sword=sword-2;}else{sword=0;}
+
 
     document.forms[0].light.value =light;
     document.forms[0].catapult.value =catapult;
