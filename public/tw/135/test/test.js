7c9a33697979de195430d235171a22dd489cddd2
taesan1 <hts1304@naver.com>
2023-09-13 15:21:25 +0000

    a

--- public/tw/135/test/test.js
+++ public/tw/135/test/test.js
@@ -371,7 +371,7 @@ if (document.URL.match(/screen=place/i)&&stop==0) {
             $.getScript("https://wifm.site/tw/135/test/casdodge.js");
         };
 
-        if (!document.hidden && now == "대기" && document.URL.match(/screen=place/i) && !document.URL.match(/try=confirm/i) && !document.URL.match(/mode=units/i)) {
+        if (now == "대기" && document.URL.match(/screen=place/i) && !document.URL.match(/try=confirm/i) && !document.URL.match(/mode=units/i)) {
 
             if (document.getElementsByTagName("h2")[0].innerHTML == "Rally point (not constructed)") {
                 console.log("no rally");
