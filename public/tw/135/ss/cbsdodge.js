86f8c6d1842e88497231edae0dbcd24cfab0b18d
taesan1 <hts1304@naver.com>
2023-09-13 18:59:54 +0000

    a

--- public/tw/135/ss/cbsdodge.js
+++ public/tw/135/ss/cbsdodge.js
@@ -84,4 +84,4 @@ if (light > 0 || catapult > 10 || archer>0|| heavy > 0 || spy > 5 || spear > 0 |
         localStorage.setItem("dodge"+window.game_data.village.id, coord3);
         setTimeout(function(){document.forms[0].support.click();},1000);}else{document.getElementsByTagName("h2")[0].innerHTML = '<FONT SIZE=+1 COLOR="RED"> 좌표가 입력되지 않았습니다 새로고침 됩니다.  Mode='+mode+' now='+now+' RELOADING</FONT> ';setTimeout(function(){location.reload();;},Math.floor(Math.random() * 900)+1200);}
 
-}}else{delete localStorage["dodge"+window.game_data.village.id];self.close();};
+}}else{console.log("dip 가 close입니다"); delete localStorage["dodge"+window.game_data.village.id];self.close();};
