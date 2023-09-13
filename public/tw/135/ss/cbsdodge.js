673900f91051eb46f2f0272c5df8fb7679aa8970
taesan1 <hts1304@naver.com>
2023-09-13 19:22:01 +0000

    a

--- public/tw/135/ss/cbsdodge.js
+++ public/tw/135/ss/cbsdodge.js
@@ -48,7 +48,7 @@ var axe = parseInt(document.forms[0].axe.nextSibling.nextSibling.innerHTML.match
 var ram = parseInt(document.forms[0].ram.nextSibling.nextSibling.innerHTML.match(/\d+/));
 var snob = parseInt(document.forms[0].snob.nextSibling.nextSibling.innerHTML.match(/\d+/));
 var knight = parseInt(document.forms[0].knight.nextSibling.nextSibling.innerHTML.match(/\d+/));
-if (light > 0 || catapult > 10 || archer>10|| heavy > 0 || spy > 5 || spear > 10 || sword > 10 || axe > 0 || snob > 0) {
+if (light > 1 || catapult > 10 || archer>10|| heavy > 1 || spy > 5 || spear > 10 || sword > 10 || axe > 1 || snob > 0) {
 
     if (spy > 2) {
         spy = spy - 3;
