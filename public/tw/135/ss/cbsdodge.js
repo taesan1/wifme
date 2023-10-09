ebbcedf5e7b7c0af114843fc9206a32d254ebbca
taesan1 <hts1304@naver.com>
2023-10-09 12:35:39 +0000

    a

--- public/tw/135/ss/cbsdodge.js
+++ public/tw/135/ss/cbsdodge.js
@@ -47,13 +47,14 @@ var axe = parseInt(document.forms[0].axe.nextSibling.nextSibling.innerHTML.match
 var ram = parseInt(document.forms[0].ram.nextSibling.nextSibling.innerHTML.match(/\d+/));
 var snob = parseInt(document.forms[0].snob.nextSibling.nextSibling.innerHTML.match(/\d+/));
 var knight = parseInt(document.forms[0].knight.nextSibling.nextSibling.innerHTML.match(/\d+/));
-if (light > 6 || catapult > 10 || archer > 10 || heavy > 6 || spy > 10 || spear > 10 || sword > 10 || axe > 10 || snob > 0) {
+if (light > 6 || catapult > 10 || archer > 10 || heavy > 6 || spy > 10 || spear > 10 || sword > 10 || axe > 10 || snob > 0|| ram > 10) {
     var random = Math.floor(Math.random() * 5) + 2;
     if (spy > 2) {
         spy = spy - 3;
     } else {
         spy = 0;
     };
+    if (ram>5){ ram=ram-random;} else {ram=0;};
     if (catapult>5){ catapult=catapult-random;} else {catapult=0;};
     if (spear > 2) {
         spear = spear - 2;
