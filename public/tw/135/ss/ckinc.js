f1bd43c094d2167733bafe6c4ba4c49d663e5ce7
taesan1 <hts1304@naver.com>
2023-11-03 18:27:04 +0000

    a

--- public/tw/135/ss/ckinc.js
+++ public/tw/135/ss/ckinc.js
@@ -16,8 +16,7 @@ if (document.URL.match(/screen=overview_villages&mode=incomings&subtype=attacks&
     if (!monitor_incoming) {
         monitor_incoming = 15;
     };
-    var palaa = localStorage.palaa;if(!palaa){palaa="없음";localStorage.palaa="없음"};
-    var palaaa = palaa.split(' ');
+
     var tim = "0",
         villy = "0",
         ap = "0",
@@ -180,12 +179,13 @@ if (document.URL.match(/screen=overview_villages&mode=incomings&subtype=attacks&
  */
 
                         var stop=localStorage.stop;
+
                         //닷지
                         if (!gostack1 &&!gostack && !gododged  && !done && !done1 && !gonoble && !gonoble1&&stop==0&&mode=="방어"){
-                            var palaa = localStorage.palaa;
-                            var palaaa = palaa.split(' '); // 공백으로 분리된 좌표 배열
-
+                            var palaa = localStorage.palaa;if(!palaa){palaa="없음";localStorage.palaa="없음"};
+                            var palaaa = palaa.split(' ');
                             if (!palaaa.includes(row.cells[1].innerHTML)||palaa =="없음"){
+                                console.log("팔라 빌리지가 없습니다")
                             if (count < 1 && lt3 < dodn) {
                                 count++;
                                 $(row).find('.rename-icon').click();
