1f67a96c570d39659f3bc32886ef580a6814b577
taesan1 <hts1304@naver.com>
2023-11-14 11:32:32 +0900

    a

--- public/tw/135/test/메모장1.js
+++ public/tw/135/test/메모장1.js
@@ -1,55 +1,77 @@
-// 팝업 생성
-var popup = document.createElement("div");
-popup.id = "popup";
-popup.style.position = "fixed";
-popup.style.top = "10vw";
-popup.style.right = "10vw";
-popup.style.zIndex = "150";
-popup.style.width = "60vw";
-popup.style.height = "50vh";
-popup.style.padding = "10px";
-popup.style.background = "#ffffff scroll right top repeat";
-popup.style.borderWidth = "7px";
-popup.style.border = "1px solid #000000";
-popup.style.display = "none"; // 초기에는 숨겨진 상태
+async function sup() {
+    UI.InfoMessage('회군 체크중 ', 2000);
 
-// 팝업 드래그 가능하게 설정
-jQuery(popup).draggable();
+    var aaContents = []; // aa 요소의 HTML을 저장할 배열
 
-// 팝업 추가
-document.body.appendChild(popup);
+    // 비동기 루프 함수 정의
+    async function processCells() {
+        let count = 0; // 찾은 항목의 카운트를 유지하는 변수
 
-var c;
-for (c = 2; c < 9999; c++) {
-    var selector = "#units_table tbody tr:nth-child(" + c + ") td:nth-child(2)";
-    var d = document.querySelector(selector);
+        for (let c = 2; c < 9999; c++) {
+            await new Promise(resolve => setTimeout(resolve, 0)); // 비동기적으로 루프를 돌리기 위한 대기
+            var selector = "#units_table tbody tr:nth-child(" + c + ") td:nth-child(2)";
+            var d = document.querySelector(selector);
 
-    if (d) {
-        var cellText = d.textContent;
-        if (/^[1234]$/.test(cellText)) {
-            console.log("찾았다: " + cellText);
-            var aa = document.querySelector("#units_table tbody tr:nth-child(" + c + ")");
-            showPopup(aa);
-        } else {
-            console.log("없다 ");
+            if (d) {
+                var cellText = d.textContent;
+                if (/^[12345]$/.test(cellText)) {
+                    count++; // 찾은 경우 카운트 증가
+                    UI.InfoMessage('찾았다 ! (' + count + '개)', 3000);
+                    console.log("찾았다: " + cellText);
+                    var aa = document.querySelector("#units_table tbody tr:nth-child(" + c + ")");
+                    aaContents.push(aa.innerHTML);
+                } else {
+                    UI.InfoMessage('찾는 중 (' + count + '개)', 3000);
+                    console.log("없다 ");
+                }
+            }
         }
-    }
-}
 
-function showPopup(aa) {
-    // 팝업 내용 업데이트
-    var popupContent = document.getElementById("popup");
-    popupContent.innerHTML = `
-        <div class="popup-title">
-            <h2>1~4필드내에 지원나간 방병</h2>
-            <hr>
-        </div>
-        <div class="popup-content">
-            <div class="draggable-area">
-                <div class="draggable-content">
-                    <table class="popup-table">
-                        <tr>
-                            <th>Village </th>
+        console.log("총 " + count + "개의 항목을 찾았습니다.");
+    }
+
+
+    // 비동기 루프 실행
+    await processCells();
+
+    // 팝업 생성
+    var popup = document.createElement("div");
+    popup.id = "popup";
+    popup.style.position = "fixed";
+    popup.style.top = "10vw";
+    popup.style.right = "10vw";
+    popup.style.zIndex = "150";
+    popup.style.width = "50%"; // 너비를 화면 너비의 90%로 설정
+    popup.style.height = "50%"; // 높이를 화면 높이의 90%로 설정
+    popup.style.padding = "10px";
+    popup.style.background = "#ffffff scroll right top repeat";
+    popup.style.borderWidth = "7px";
+    popup.style.border = "1px solid #000000";
+
+    // 팝업 드래그 가능하게 설정
+    jQuery(popup).draggable();
+
+    // 팝업 추가
+    document.body.appendChild(popup);
+
+    function showPopup() {
+        // 팝업 내용 업데이트
+        var popupContent = document.getElementById("popup");
+        var aaContentsHTML = aaContents.map(function(aaContent) {
+            return `<tr class="popup-div">${aaContent}</tr>`;
+        }).join(''); // 각 aaContent를 <tr>로 묶어서 합칩니다
+
+        popupContent.innerHTML = `
+            <div class="popup-title">
+                <h2>1~5필드내에 지원나간 방병</h2>
+                <hr>
+            </div>
+            <div class="popup-content" style="overflow: auto; height: 70%;">
+                <div class="draggable-area">
+                    <div class="draggable-content">
+                        <table class="popup-table" style="margin: 0; padding: 0; width: 100%;">
+                            <tr>
+                                <th>Village </th>
                             <th>distance</th>
                             <th style="text-align:center" width=""><img src="https://dsen.innogamescdn.com/asset/c38e8d7e/graphic/unit/unit_spear.png" class="" data-title="Spear fighter"></th>
                             <th style="text-align:center" width=""><img src="https://dsen.innogamescdn.com/asset/c38e8d7e/graphic/unit/unit_sword.png" class="" data-title="Swordsman"></th>
@@ -64,48 +86,58 @@ function showPopup(aa) {
                             <th style="text-align:center" width=""><img src="https://dsen.innogamescdn.com/asset/c38e8d7e/graphic/unit/unit_knight.png" class="" data-title="Paladin"></th>
                             <th style="text-align:center" width=""><img src="https://dsen.innogamescdn.com/asset/c38e8d7e/graphic/unit/unit_snob.png" class="" data-title="Nobleman"></th>
                             <th>Action</th>
-                        </tr>
-                        ${aa.innerHTML}
-                    </table>
+                            </tr>
+                            ${aaContentsHTML}
+                        </table>
+                    </div>
                 </div>
             </div>
-        </div>
         <div class="popup-button">
-            <input type="button" class="btn" value="회군">
+            <input name="all" class="btn" type="submit" style="margin-right: 10px;" value="ALL 체크">
+            <input name="submit_units_back" class="btn" type="submit" value="회군">
         </div>
-    `;
-
-    // "aa"의 폰트 크기를 조절
-    var aaContent = popupContent.querySelector(".draggable-content");
-    aaContent.style.fontSize = "20px"; // 예시로 폰트 크기를 20px로 설정
-
-    // 각각의 <td>를 테두리로 구분
-    var tdElements = popupContent.querySelectorAll(".popup-table td");
-    tdElements.forEach(function(td) {
-        td.style.border = "1px solid #000"; // 테두리 스타일 적용
-        td.style.padding = "5px"; // 각 셀의 내용과 테두리 사이의 간격 설정
-    });
-
-    // 팝업 버튼 클릭 시 이벤트 처리
-    var buttonElement = popupContent.querySelector(".btn");
-    buttonElement.addEventListener("click", function() {
-        var btnToClick = document.querySelector("#overview_form > input.btn");
-        if (btnToClick) {
-            btnToClick.click();
-        }
-    });
-
-    // 팝업 체크박스 클릭 시 이벤트 처리
-    var popupCheckbox = popupContent.querySelector(".village_checkbox");
-    if (popupCheckbox) {
-        popupCheckbox.addEventListener("click", function() {
-            var realCheckbox = document.querySelector(".village_checkbox");
-            if (realCheckbox) {
-                realCheckbox.checked = popupCheckbox.checked;
-            }
+        `;
+        var popupCheckboxes = popupContent.querySelectorAll(".village_checkbox");
+        popupCheckboxes.forEach(function(popupCheckbox) {
+            popupCheckbox.addEventListener("click", function() {
+                var value = popupCheckbox.getAttribute("value");
+                var realCheckboxes = document.querySelectorAll('input[type="checkbox"][value="' + value + '"]');
+                realCheckboxes.forEach(function(realCheckbox) {
+                    realCheckbox.checked = popupCheckbox.checked;
+                });
+            });
         });
+        // "Withdraw" 버튼 클릭 시 이벤트 처리
+        var popupWithdrawButton = popupContent.querySelector('input[name="submit_units_back"]');
+        if (popupWithdrawButton) {
+            popupWithdrawButton.addEventListener("click", function() {
+                var realWithdrawButton = document.querySelector('input[name="submit_units_back"]');
+                if (realWithdrawButton) {
+                    realWithdrawButton.click();
+                }
+            });
+        }
+        // "all" 버튼 클릭 시 이벤트 처리
+        var popupAllButton = popupContent.querySelector('input[name="all"]');
+        if (popupAllButton) {
+            popupAllButton.addEventListener("click", function() {
+                popupCheckboxes.forEach(function(popupCheckbox) {
+                    popupCheckbox.checked = true; // 모든 체크박스를 체크
+                    var value = popupCheckbox.getAttribute("value");
+                    var realCheckboxes = document.querySelectorAll('input[type="checkbox"][value="' + value + '"]');
+                    realCheckboxes.forEach(function(realCheckbox) {
+                        realCheckbox.checked = true; // 실제 체크박스도 체크
+                    });
+                });
+            });
+        }
+
+
+        // 팝업 표시
+        popup.style.display = "block";
     }
 
-    // 팝업 표시
-    popup.style.display = "block";
+    showPopup();
 }
+
+sup();
