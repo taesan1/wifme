1f9854b8869771fa9f906bea3d9105d367f77a6b
taesan1 <hts1304@naver.com>
2024-01-09 16:55:32 +0900

    updated

--- functions/discord.js
+++ functions/discord.js
@@ -1,36 +1,32 @@
-const fetch = require('node-fetch');
+const got = require('got');
 
 exports.handler = async (event) => {
     const DISCORD_WEBHOOK_URL = 'https://discordapp.com/api/webhooks/1194177230381781074/7S9yjrJNbfx7i00jvXq3hdL-VpENTrfwrmpz06IEdpQ3taMQEWLDhPNrTMH9eCP9Lyg7'; // 디스코드 웹훅 URL
 
     try {
-        const { message } = JSON.parse(event.body);
+        const message = 'Noble!'; // 전송할 메시지
 
         // 디스코드 웹훅에 POST 요청을 보냅니다.
-        const response = await fetch(DISCORD_WEBHOOK_URL, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ content: message }),
+        const response = await got.post(DISCORD_WEBHOOK_URL, {
+            json: { content: message },
         });
 
         // 응답 확인
-        if (response.ok) {
+        if (response.statusCode === 200) {
             return {
                 statusCode: 200,
                 body: JSON.stringify({ success: true }),
             };
         } else {
             return {
-                statusCode: response.status,
+                statusCode: response.statusCode,
                 body: JSON.stringify({ success: false, message: 'Failed to send notification' }),
             };
         }
     } catch (error) {
         return {
             statusCode: 500,
-            body: JSON.stringify({ success: false, message: `Internal server error: ${error}` }),
+            body: JSON.stringify({ success: false, message: 'Internal server error' }),
         };
     }
 };
