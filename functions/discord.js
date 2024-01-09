c870580fd38e6dbe9fb25eac2e0febc45692d4e5
taesan1 <hts1304@naver.com>
2024-01-09 16:46:16 +0900

    updated

--- functions/discord.js
+++ functions/discord.js
@@ -1,12 +1,8 @@
-// netlify/functions/discordNotification.js
-
-const fetch = require('node-fetch');
-
 exports.handler = async (event) => {
     const DISCORD_WEBHOOK_URL = 'https://discordapp.com/api/webhooks/1194177230381781074/7S9yjrJNbfx7i00jvXq3hdL-VpENTrfwrmpz06IEdpQ3taMQEWLDhPNrTMH9eCP9Lyg7'; // 디스코드 웹훅 URL
 
     try {
-        const message = 'Noble!'; // 전송할 메시지
+        const message = JSON.parse(event.body).message || 'Default message'; // 요청 본문에서 메시지 가져오기
 
         // 디스코드 웹훅에 POST 요청을 보냅니다.
         const response = await fetch(DISCORD_WEBHOOK_URL, {
