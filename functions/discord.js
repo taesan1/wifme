c788eb3cfa2e7cd650b8af121a41fcbb05c56c05
taesan1 <hts1304@naver.com>
2024-01-09 16:50:11 +0900

    updated

--- functions/discord.js
+++ functions/discord.js
@@ -1,8 +1,10 @@
+const fetch = require('node-fetch');
+
 exports.handler = async (event) => {
     const DISCORD_WEBHOOK_URL = 'https://discordapp.com/api/webhooks/1194177230381781074/7S9yjrJNbfx7i00jvXq3hdL-VpENTrfwrmpz06IEdpQ3taMQEWLDhPNrTMH9eCP9Lyg7'; // 디스코드 웹훅 URL
 
     try {
-        const message = JSON.parse(event.body).message || 'Default message'; // 요청 본문에서 메시지 가져오기
+        const { message } = JSON.parse(event.body);
 
         // 디스코드 웹훅에 POST 요청을 보냅니다.
         const response = await fetch(DISCORD_WEBHOOK_URL, {
@@ -28,7 +30,7 @@ exports.handler = async (event) => {
     } catch (error) {
         return {
             statusCode: 500,
-            body: JSON.stringify({ success: false, message: 'Internal server error' }),
+            body: JSON.stringify({ success: false, message: `Internal server error: ${error}` }),
         };
     }
 };
