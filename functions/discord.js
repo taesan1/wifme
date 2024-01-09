e64a6fb3c0abc1533aa2fc92b8125310f7e5683e
taesan1 <hts1304@naver.com>
2024-01-09 17:17:21 +0900

    updated

--- functions/discord.js
+++ functions/discord.js
@@ -4,7 +4,9 @@ exports.handler = async (event) => {
     const DISCORD_WEBHOOK_URL = 'https://discordapp.com/api/webhooks/1194177230381781074/7S9yjrJNbfx7i00jvXq3hdL-VpENTrfwrmpz06IEdpQ3taMQEWLDhPNrTMH9eCP9Lyg7'; // 디스코드 웹훅 URL
 
     try {
-        const message = 'Noble!'; // 전송할 메시지
+        // 사용자로부터 전송할 메시지를 받아옵니다.
+        const userInput = JSON.parse(event.body);
+        const message = userInput.message;
 
         // 디스코드 웹훅에 POST 요청을 보냅니다.
         const response = await axios.post(DISCORD_WEBHOOK_URL, { content: message });
